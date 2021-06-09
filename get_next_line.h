301fb1cf55320e3300a412e835e25cee80a550b1
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-08 22:15:08 -0300

    add files

--- get_next_line.h
+++ get_next_line.h
@@ -5,6 +5,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# define FD_SIZE 4096
+
 int		get_next_line(int fd, char **line);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
