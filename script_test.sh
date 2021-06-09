d1d42eb24a04c2aa40d8a25657f72d78105d96cf
MarcusVinix <vi.ni159marcus@gmail.com>
2021-06-09 10:40:23 -0300

    add files

--- script_test.sh
+++ script_test.sh
@@ -1,3 +1,12 @@
+# INSTALL VALGRIND
+cd ~
+rm -rf $HOME/.brew &&
+	git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew &&
+	echo 'export PATH=$HOME/.brew/bin:$PATH' >>$HOME/.zshrc &&
+	source $HOME/.zshrc &&
+	brew update
+curl https://raw.githubusercontent.com/sowson/valgrind/master/valgrind.rb >valgrind.rb
+brew install --HEAD valgrind.rb
 
 # CLONE YOUR PROJECT
 cd ~
